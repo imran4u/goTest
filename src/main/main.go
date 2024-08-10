51ba2b6aac97df45364350722260c84d20594f47
imran4u <er.imran4u@gmail.com>
2024-08-10 21:26:37 +0530

    Pass by value and pass by ref example

--- src/main/main.go
+++ src/main/main.go
@@ -3,7 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	secondLine := "2nd line"
-	fmt.Println("Hello world brother")
-	fmt.Println(secondLine)
+	// 5, 8 alishan : wrong
+
+	// var a int = 5
+	a := 5
+	fmt.Printf("value of a %d\n", a)
+	changeValue(a)
+	fmt.Printf("after changeValue: value of a %d\n", a)
+
+	changeByRef(&a)
+	fmt.Printf("After changeBy ref: value of a %d\n", a) //9
+	changeByRef(&a)
+	fmt.Printf("After changeBy ref: value of a %d\n", a) //13
+}
+
+func changeValue(a int) {
+	a = a + 3
+	fmt.Printf("Inside changeValue: value of a %d\n", a)
+}
+
+func changeByRef(a *int) {
+	*a = *a + 4
 }
