2ce414738da22fea8afafbe01d056b764efd87d7
imran4u <er.imran4u@gmail.com>
2024-08-10 21:46:21 +0530

    Slice and array example

--- src/main/main.go
+++ src/main/main.go
@@ -3,7 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	secondLine := "2nd line"
-	fmt.Println("Hello world brother")
-	fmt.Println(secondLine)
+	a := []int{1, 2, 3}
+
+	fmt.Printf("Hello \n")
+	fmt.Print(a)
+	fmt.Printf("\n---------------------\n")
+
+	a[2] = 6
+	fmt.Print(a)
+
+	fmt.Print("\n------\n")
+	a = append(a, 8)
+	fmt.Print(a)
+	fmt.Print("\n------\n")
+
+	//----------------
+
+	arr1 := [6]int{10, 11, 12, 13, 14, 15} //Array
+	myslice := arr1[1:5]
+
+	fmt.Printf("myslice = %v\n", myslice)
+	fmt.Printf("length = %d\n", len(myslice))
+	fmt.Printf("capacity = %d\n", cap(myslice))
+
 }
