db030605fca4e71f59a981bbdd208b0a153db593
imran4u <er.imran4u@gmail.com>
2024-08-12 21:39:32 +0530

    Map exmaple

--- src/main/main.go
+++ src/main/main.go
@@ -3,7 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	secondLine := "2nd line"
-	fmt.Println("Hello world brother")
-	fmt.Println(secondLine)
+	// Map:
+	// 1. Create
+	// 2. Get
+	// 3. Set
+	// 4. Delete
+	// 5. Iterate
+	// 6. Length
+	// 7. Copy
+	// 8. Merge
+
+	mapObj := make(map[string]string)
+	mapObj["gr"] = "Grape fruit"
+	mapObj["app"] = "Apple fruit"
+	mapObj["or"] = "Orange fruit"
+	mapObj["ki"] = "Kiwi fruit"
+
+	// Iterate
+	for key, value := range mapObj {
+		fmt.Println(key, ":", value)
+	}
+
+	fmt.Println(mapObj)
+	fmt.Println(mapObj["gr"])
+
 }
