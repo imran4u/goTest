b8bda7be01ceb3e13d459d5cbae8f64423c4be08
imran4u <er.imran4u@gmail.com>
2024-08-14 21:23:46 +0530

    Defer example with stack concept

--- src/main/main.go
+++ src/main/main.go
@@ -3,7 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	defer funOne()
+	defer funTwo()
+
+	defer func(end string) {
+		fmt.Println(end)
+	}("The end")
+
 	secondLine := "2nd line"
 	fmt.Println("Hello world brother")
 	fmt.Println(secondLine)
+
+	// Hello world brother
+	// 2nd line
+	// The end
+	// funTwo
+	// funOne
+
+}
+
+func funOne() {
+	fmt.Println("funOne")
+}
+
+func funTwo() {
+	fmt.Println("funTwo")
 }
