b9ba3619541a69dcaf5cdadd06b549e10aff21df
imran4u <er.imran4u@gmail.com>
2024-08-14 21:39:43 +0530

    go routine to check links

--- src/main/main.go
+++ src/main/main.go
@@ -1,31 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	defer funOne()
-	defer funTwo()
 
-	defer func(end string) {
-		fmt.Println(end)
-	}("The end")
+	links := []string{"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://youtube-wrong.com",
+		"http://amazon.com",
+		"http://wikipedia.org",
+		"http://twitter.com",
+	}
+	startTime := time.Now()
 
-	secondLine := "2nd line"
-	fmt.Println("Hello world brother")
-	fmt.Println(secondLine)
+	for _, link := range links {
+		go checkLink(link)
+	}
 
-	// Hello world brother
-	// 2nd line
-	// The end
-	// funTwo
-	// funOne
+	endTime := time.Now()
+
+	fmt.Println("Total Time: ", endTime.Sub(startTime))
+	time.Sleep(20 * time.Second)
 
 }
 
-func funOne() {
-	fmt.Println("funOne")
-}
-
-func funTwo() {
-	fmt.Println("funTwo")
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is down!")
+		return
+	}
+	fmt.Println(link, "is up!")
 }
