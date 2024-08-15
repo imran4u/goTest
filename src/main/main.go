f5c0c1a992944ea13f48b1cf49d1e0026be271dd
imran4u <er.imran4u@gmail.com>
2024-08-15 21:03:41 +0530

    WaitGroup example

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -17,24 +18,40 @@ func main() {
 		"http://wikipedia.org",
 		"http://twitter.com",
 	}
+
 	startTime := time.Now()
-
 	for _, link := range links {
-		go checkLink(link)
+		checkLink(link, nil)
 	}
-
 	endTime := time.Now()
 
-	fmt.Println("Total Time: ", endTime.Sub(startTime))
-	time.Sleep(20 * time.Second)
+	fmt.Println("Without goRoutine Total Time: ", endTime.Sub(startTime))
+
+	// Using Go routine
+	wg := &sync.WaitGroup{}
+	wg.Add(len(links))
+	startTime = time.Now()
+	for _, link := range links {
+		go checkLink(link, wg)
+	}
+	endTime = time.Now()
+
+	wg.Wait()
+	fmt.Println("With goRoutine Total Time: ", endTime.Sub(startTime))
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, wg *sync.WaitGroup) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is down!")
+		if wg != nil {
+			wg.Done()
+		}
 		return
 	}
 	fmt.Println(link, "is up!")
+	if wg != nil {
+		wg.Done()
+	}
 }
