39f5c7d8a78ec003707d6b2ea1b617e487b7615d
imran4u <er.imran4u@gmail.com>
2024-08-16 21:59:50 +0530

    interface exmaple

--- src/main/main.go
+++ src/main/main.go
@@ -50,4 +50,44 @@ func main() {
 	fmt.Println("high speed:", v.highSpeed())
 	fmt.Println(v)
 
+	// assigns value to struct members
+	rect := Rectangle{7, 4, "rectangle"}
+	circ := Circle{5, "circle"}
+	// call calculate() with struct variable rect
+	calculate(rect)
+	calculate(circ)
+
+}
+
+//2nd Interface
+
+// interface
+type Shape interface {
+	area() float32
+}
+
+// struct to implement interface
+type Rectangle struct {
+	length, breadth float32
+	name            string
+}
+
+// use struct to implement area() of interface
+func (r Rectangle) area() float32 {
+	return r.length * r.breadth
+}
+
+type Circle struct {
+	radius float32
+	name   string
+}
+
+func (c Circle) area() float32 {
+	return 3.14 * c.radius * c.radius
+}
+
+// access method of the interface
+func calculate(s Shape) {
+	fmt.Println(s)
+	fmt.Println("Area:", s.area())
 }
