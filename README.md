d449067e708fdbfc50b06b72e1ed285298fc0ebc
imran4u <er.imran4u@gmail.com>
2024-08-17 19:26:02 +0530

    Add go cli program for mangodb

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Mango DB example
 
 ### steps: 
-1. Run command 
+1. Run command to install Mango driver
 
     `go get go.mongodb.org/mongo-driver`
+2. To install CLI command
+
+    `go get github.com/urfave/cli/v2`
+
+3. start mangodb on your machine [ref](https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/)
+
+    `sudo systemctl start mongod`
+4. stop mangodb
+
+    `sudo systemctl stop mongod`
--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/urfave/cli/v2"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("tasker").Collection("tasks")
+}
 func main() {
-	secondLine := "2nd line"
-	fmt.Println("Hello world brother")
-	fmt.Println(secondLine)
+	app := &cli.App{
+		Name:     "tasker",
+		Usage:    "A simple CLI program to manage your tasks",
+		Commands: []*cli.Command{},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
