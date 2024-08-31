8169e9ecfa3d52076b19bab571b828d4912a2116
imran4u <er.imran4u@gmail.com>
2024-08-31 15:29:25 +0530

    Recursive function

--- src/main/main.go
+++ src/main/main.go
@@ -6,4 +6,17 @@ func main() {
 	secondLine := "2nd line"
 	fmt.Println("Hello world brother")
 	fmt.Println(secondLine)
+
+	fmt.Println("\nrecursive output:")
+	fmt.Println(recursive(-50))
+}
+
+func recursive(n int) int {
+	if n < 1 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return n + recursive(n-1)
 }
