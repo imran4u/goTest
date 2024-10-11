8bb1995a28b045ef1d62477859599b3a68162291
imran4u <er.imran4u@gmail.com>
2024-10-11 19:54:27 +0530

    Linked list add fuction

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//Linked list
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (l Node) String() string {
+	return fmt.Sprintf("%d ", l.value)
+}
+
+// Add at end
+func (l *LinkedList) Add(value int) {
+	node := &Node{value: value}
+	if l.head == nil {
+		l.head = node
+	} else {
+		for iterator := l.head; iterator != nil; iterator = iterator.next {
+			if iterator.next == nil {
+				iterator.next = node
+				break
+			}
+		}
+	}
+}
+
+// Add from beginning
+func (l *LinkedList) AddFromStart(value int) {
+	node := &Node{value: value}
+	node.next = l.head
+	l.head = node
+}
+
+func (l LinkedList) PrintVertically() {
+	node := l.head
+	for node != nil {
+		fmt.Println(node)
+		node = node.next
+	}
+}
+
+func (l LinkedList) Print() {
+	sb := strings.Builder{}
+
+	for iterator := l.head; iterator != nil; iterator = iterator.next {
+		sb.WriteString(iterator.String())
+	}
+	fmt.Println(sb.String())
+}
+
+func main() {
+
+	// LinkedList := new(LinkedList) //LinkedList
+	// LinkedList := LinkedList{head: nil}
+	// LinkedList := LinkedList{}
+	linkedList := LinkedList{}
+
+	linkedList.Add(1)
+	linkedList.Add(2)
+	linkedList.Add(3)
+	linkedList.Print()
+}
